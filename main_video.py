542f09e03270393bdc99a68394ce8619b53b06ca
kcgkcg <131759676+kcgpanfan@users.noreply.github.com>
2023-12-22 14:33:06 +0800

    main_video.py

--- main_video.py
+++ main_video.py
@@ -1,75 +1,80 @@
 #! /usr/bin/env python
 import os
 import cv2
-import logging
+import numpy
 import argparse
 import streamlit as st
-import numpy as np
+from PIL import Image, ImageEnhance
 
-from face_detection import select_face
+from face_detection import select_face, select_all_faces
 from face_swap import face_swap
 
-
-class VideoHandler(object):
-    def __init__(self, video_path=0, img_path=None, args=None):
-        self.src_points, self.src_shape, self.src_face = select_face(cv2.imread(img_path))
-        if self.src_points is None:
-            st.write('No face detected in the source image !!!')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='FaceSwapApp')
+    parser.add_argument('--correct_color', default=True, action='store_true', help='Correct color')
+    parser.add_argument('--warp_2d', default=False, action='store_true', help='2d or 3d warp')
+    args = parser.parse_args()
+    
+    uploaded_source_file = st.file_uploader("Source File", type=['jpg','png','jpeg'])
+    uploaded_target_file = st.file_uploader("Target File", type=['jpg','png','jpeg'])
+    
+    if uploaded_source_file is not None and uploaded_target_file is not None:
+        source_image = Image.open(uploaded_source_file)
+        target_image = Image.open(uploaded_target_file)
+        
+        # Convert images from PIL to CV2
+        src_img = cv2.cvtColor(numpy.array(source_image), cv2.IMREAD_COLOR)
+        dst_img = cv2.cvtColor(numpy.array(target_image), cv2.IMREAD_COLOR)
+        
+        # Select src face
+        src_points, src_shape, src_face = select_face(src_img)
+        # Select dst face
+        dst_faceBoxes = select_all_faces(dst_img)
+        
+        if dst_faceBoxes is None:
+            print('Detect 0 Face !!!')
             exit(-1)
-        self.args = args
-        self.video = cv2.VideoCapture(video_path)
-        self.writer = cv2.VideoWriter(args.save_path, cv2.VideoWriter_fourcc(*'MJPG'),
-                                      self.video.get(cv2.CAP_PROP_FPS),
-                                      (int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                                       int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+        
+        output = dst_img
+        for k, dst_face in dst_faceBoxes.items():
+            output = face_swap(src_face, dst_face["face"], src_points,
+                               dst_face["points"], dst_face["shape"],
+                               output, args)
+            
+            st.markdown('<p style="text-align: left;">Result</p>',unsafe_allow_html=True)
+            st.image(output, width=500)
 
-    def start(self):
-        while self.video.isOpened():
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+uploaded_video_file = st.file_uploader("Video File", type=['mp4','avi','mov'])
 
-            _, dst_img = self.video.read()
-            dst_points, dst_shape, dst_face = select_face(dst_img, choose=False)
-            if dst_points is not None:
-                dst_img = face_swap(self.src_face, dst_face, self.src_points, dst_points, dst_shape, dst_img, self.args, 68)
-            self.writer.write(dst_img)
-            if self.args.show:
-                st.image(dst_img, channels="BGR")
-
-        self.video.release()
-        self.writer.release()
-        cv2.destroyAllWindows()
-
-
-logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(lineno)d:%(message)s")
-
-st.title('Face Swap Video')
-
-uploaded_source_file = st.file_uploader("Source Image", type=['jpg', 'png', 'jpeg'])
-uploaded_video_file = st.file_uploader("Video", type=['mp4', 'avi', 'mov'])
-
-if uploaded_source_file is not None and uploaded_video_file is not None:
-    source_image = cv2.imread(uploaded_source_file.name)
-    video_path = uploaded_video_file.name
-    save_path = st.text_input("Output Video Path", value="output.mp4")
-
-    st.write('Source Image:')
-    st.image(source_image, channels="BGR")
-
-    st.write('Video:')
-    st.video(uploaded_video_file)
-
-    if st.button('Start Face Swap'):
-        with st.spinner('Processing...'):
-            args = argparse.Namespace(
-                src_img=uploaded_source_file.name,
-                video_path=video_path,
-                warp_2d=False,
-                correct_color=False,
-                show=False,
-                save_path=save_path
-            )
-            VideoHandler(video_path, uploaded_source_file.name, args).start()
-        st.success('Face swap completed!')
-
-st.write('Please upload a source image and a video file to start the face swap process.')
+if uploaded_video_file is not None:
+    video_path = "temp_video.mp4"
+    with open(video_path, "wb") as file:
+        file.write(uploaded_video_file.read())
+    
+    cap = cv2.VideoCapture(video_path)
+    out = cv2.VideoWriter("output_video.mp4", cv2.VideoWriter_fourcc(*"mp4v"),
+                          cap.get(cv2.CAP_PROP_FPS), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))),
+                          int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        dst_faceBoxes = select_all_faces(frame)
+        
+        if dst_faceBoxes is not None:
+            for k, dst_face in dst_faceBoxes.items():
+                output = face_swap(src_face, dst_face["face"], src_points,
+                                   dst_face["points"], dst_face["shape"],
+                                   frame, args)
+                out.write(output)
+        else:
+            out.write(frame)
+    
+    cap.release()
+    out.release()
+    
+    st.markdown('<p style="text-align: left;">Result</p>',unsafe_allow_html=True)
+    st.video("output_video.mp4")
